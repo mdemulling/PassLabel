33617fba95ed939c9153ac6d8ffa5f3b1d9b62fb
demul <demul@LAPTOP-51FI2UIM>
2021-11-12 02:33:42 -0600

    Add project files.

---
+++ PassLabel/Trying.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassLabel
+{
+    public class Trying
+    {
+        public void Treping(Form1 frm)
+        {
+            frm.label1.Text = "Boogie Booogie now now";
+
+
+
+
+        }
+    }
+}
